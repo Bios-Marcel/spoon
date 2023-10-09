2d3ac28d8cfc6a56cf915120d9651787fb51ff06
Marcel Schramm <marceloschr@googlemail.com>
2023-10-09 22:09:28 +0200

    Add update, install and uninstall including autocomplete

--- cmd/spoon/main.go
+++ cmd/spoon/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"runtime"
 	"slices"
@@ -16,9 +18,92 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func execScoopCommand(command string, args ...string) int {
+	cmd := exec.Command("scoop", append([]string{command}, args...)...)
+	fmt.Println(cmd.String())
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+
+	if err := cmd.Run(); err != nil {
+		return 1
+	}
+
+	return 0
+}
+
+func getFlags(cmd *cobra.Command, flags ...string) ([]string, error) {
+	var outFlags []string
+	for _, name := range flags {
+		if !cmd.Flags().Changed(name) {
+			continue
+		}
+
+		flag := cmd.Flags().Lookup(name)
+		switch flag.Value.Type() {
+		case "bool":
+			outFlags = append(outFlags, "--"+name)
+		default:
+			outFlags = append(outFlags, "--"+name, flag.Value.String())
+		}
+	}
+
+	return outFlags, nil
+}
+
 var (
 	rootCmd = cobra.Command{
 		Use: "spoon",
+	}
+
+	uninstallCmd = cobra.Command{
+		Use: "uninstall",
+		Aliases: []string{
+			"remove",
+			"delete",
+			"rm",
+		},
+		Args:              cobra.MinimumNArgs(1),
+		ValidArgsFunction: autocompleteInstalled,
+		Run: func(cmd *cobra.Command, args []string) {
+			flags, err := getFlags(cmd, "global", "purge")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			os.Exit(execScoopCommand("uninstall", append(flags, args...)...))
+		},
+	}
+
+	updateCmd = cobra.Command{
+		Use: "update",
+		Aliases: []string{
+			"upgrade",
+			"up",
+		},
+		Args:              cobra.MinimumNArgs(1),
+		ValidArgsFunction: autocompleteInstalled,
+		Run: func(cmd *cobra.Command, args []string) {
+			flags, err := getFlags(cmd, "force", "global", "indepdendent", "no-cache", "skip", "quiet", "all")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			os.Exit(execScoopCommand("update", append(flags, args...)...))
+		},
+	}
+	installCmd = cobra.Command{
+		Use:               "install",
+		Args:              cobra.MinimumNArgs(1),
+		ValidArgsFunction: autocompleteAvailable,
+		Run: func(cmd *cobra.Command, args []string) {
+			flags, err := getFlags(cmd, "global", "independent", "no-cache", "no-update-scoop", "skip", "arch")
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+			os.Exit(execScoopCommand("install", append(flags, args...)...))
+		},
 	}
 
 	searchCmd = cobra.Command{
@@ -29,12 +114,7 @@ var (
 		Args:    cobra.ExactArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
 			search := args[0]
-			home, err := os.UserHomeDir()
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			dirs, err := filepath.Glob(filepath.Join(home, "scoop/buckets/*/bucket"))
+			dirs, err := getBucketDirs()
 			if err != nil {
 				fmt.Println(err)
 				return
@@ -145,13 +225,7 @@ var (
 
 			for _, dir := range dirs {
 				bucket := filepath.Base(filepath.Dir(dir))
-				dirHandle, err := os.Open(dir)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-
-				entries, err := dirHandle.Readdir(-1)
+				entries, err := getDirEntries(dir)
 				if err != nil {
 					fmt.Println(err)
 					return
@@ -209,6 +283,87 @@ var (
 	}
 )
 
+func autocompleteAvailable(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	buckets, err := getBucketDirs()
+	if err != nil {
+		fmt.Println(err)
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	toComplete = strings.ToLower(toComplete)
+	var matches []string
+	for _, bucket := range buckets {
+		manifests, err := getDirEntries(bucket)
+		if err != nil {
+			fmt.Println(err)
+			return nil, cobra.ShellCompDirectiveNoFileComp
+		}
+
+		for _, manifest := range manifests {
+			name := strings.TrimSuffix(manifest.Name(), ".json")
+			if strings.HasPrefix(name, toComplete) {
+				matches = append(matches, name)
+			}
+		}
+	}
+
+	if len(matches) == 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	return matches, cobra.ShellCompDirectiveDefault
+
+}
+
+func autocompleteInstalled(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	toComplete = strings.ToLower(toComplete)
+	var matches []string
+
+	manifests, err := getInstalledManifests()
+	if err != nil {
+		fmt.Println(err)
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+
+	for _, manifest := range manifests {
+		name := filepath.Base(filepath.Dir(filepath.Dir(manifest)))
+		if strings.HasPrefix(name, toComplete) {
+			matches = append(matches, name)
+		}
+	}
+
+	if len(matches) == 0 {
+		return nil, cobra.ShellCompDirectiveNoFileComp
+	}
+	return matches, cobra.ShellCompDirectiveDefault
+}
+
+func getBucketDirs() ([]string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("error getting home directory: %w", err)
+	}
+
+	return filepath.Glob(filepath.Join(home, "scoop/buckets/*/bucket"))
+}
+
+func getInstalledManifests() ([]string, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return nil, fmt.Errorf("error getting home directory: %w", err)
+	}
+
+	return filepath.Glob(filepath.Join(home, "scoop/apps/*/current/manifest.json"))
+}
+
+func getDirEntries(dir string) ([]fs.FileInfo, error) {
+	dirHandle, err := os.Open(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	return dirHandle.Readdir(-1)
+}
+
 // equals checks for string equality, optionally ignoring casing. The value `b`
 // is expected to be lowered already, if `ci` has been set.
 func equals(a, b string, ci bool) bool {
@@ -255,6 +410,28 @@ const (
 
 func init() {
 	rootCmd.AddCommand(&searchCmd)
+	rootCmd.AddCommand(&installCmd)
+	rootCmd.AddCommand(&uninstallCmd)
+	rootCmd.AddCommand(&updateCmd)
+
+	uninstallCmd.Flags().BoolP("global", "g", false, "Uninstall a globally installed app")
+	uninstallCmd.Flags().BoolP("purge", "p", false, "Remove all persistent data")
+
+	installCmd.Flags().BoolP("global", "g", false, "Install an app globally")
+	installCmd.Flags().BoolP("independent", "i", false, "Don't install dependencies automatically")
+	installCmd.Flags().BoolP("no-cache", "k", false, "Don't use download cache")
+	installCmd.Flags().BoolP("no-update-scoop", "u", false, "Don't use scoop before i if it's outdated")
+	installCmd.Flags().BoolP("skip", "s", false, "Skip hash validation")
+	installCmd.Flags().BoolP("arch", "a", false, "use specified architechture, if app supports it")
+
+	updateCmd.Flags().BoolP("force", "f", false, "Force update even where tehre isn't a newer version")
+	updateCmd.Flags().BoolP("global", "g", false, "Install an app globally")
+	updateCmd.Flags().BoolP("independent", "i", false, "Don't install dependencies automatically")
+	updateCmd.Flags().BoolP("no-cache", "k", false, "Don't use download cache")
+	updateCmd.Flags().BoolP("skip", "s", false, "Skip hash validation")
+	updateCmd.Flags().BoolP("quiet", "q", false, "Hide extraenous messages")
+	updateCmd.Flags().BoolP("all", "a", false, "Update all apps (alternative to '*')")
+
 	outFormat = rootCmd.PersistentFlags().String("out-format", "plain", "Specifies the output format to use for any data printed")
 
 	searchCmd.Flags().IntP("workers", "w", runtime.NumCPU(), "Sets the maximum amount of workers to do background tasks with")
--- go.mod
+++ go.mod
@@ -3,14 +3,13 @@ module github.com/Bios-Marcel/spoon
 go 1.21.1
 
 require (
+	github.com/mailru/easyjson v0.7.7
+	github.com/rodaine/table v1.1.0
 	github.com/spf13/cobra v1.7.0
-	golang.org/x/text v0.13.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
-	github.com/mailru/easyjson v0.7.7 // indirect
-	github.com/rodaine/table v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
