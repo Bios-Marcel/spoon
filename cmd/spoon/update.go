d05b1cea05a16ed3ceaf29e13659ca566eafcdd2
Marcel Schramm <marceloschr@googlemail.com>
2024-03-25 22:30:30 +0100

    spoon update now accepts an arbitrary number of args

--- cmd/spoon/update.go
+++ cmd/spoon/update.go
@@ -15,16 +15,16 @@ func updateCmd() *cobra.Command {
 			"upgrade",
 			"up",
 		},
-		Args:              cobra.MinimumNArgs(1),
+		Args:              cobra.ArbitraryArgs,
 		ValidArgsFunction: autocompleteInstalled,
-		Run: func(cmd *cobra.Command, args []string) {
+		RunE: RunE(func(cmd *cobra.Command, args []string) error {
 			flags, err := getFlags(cmd, "force", "global", "indepdendent", "no-cache", "skip", "quiet", "all")
 			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
+				return fmt.Errorf("error reading flags: %w", err)
 			}
 			os.Exit(execScoopCommand("update", append(flags, args...)...))
-		},
+			return nil
+		}),
 	}
 
 	cmd.Flags().BoolP("force", "f", false, "Force update even where tehre isn't a newer version")
